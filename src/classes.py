483aa318ca645285ef3dd547bf5139bec1c528de
konys <Konyshev_PV@bk.ru>
2024-02-29 10:33:35 +0300

    задание 2: инициализация и добавление атрибутов

--- src/classes.py
+++ src/classes.py
@@ -3,9 +3,30 @@ class Category:
     name = str
     description: str
     goods: list
+    total_numbers_of_category = 0
+    unique_goods = 0
+
+
+    def __init__(self, name, description, goods):
+        """Инициализация класса категории"""
+        self.name = name
+        self.description = description
+        self.goods = goods
+
+        Category.total_numbers_of_category += 1
+        Category.unique_goods += 1
 
 class Product:
     name: str
     description: str
     price: float
     quantity_in_stock: int
+
+    def __init__(self, name, description, price, quantity_in_stock):
+        """Инициализация класса продукт"""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
+
+
